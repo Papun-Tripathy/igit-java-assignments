de8ba87fef492ad04693fc9caea87c8a2940245b
Papun-Tripathy <papuntripathy2000@gmail.com>
2022-09-23 10:34:08 +0530

    till 6 e

---
+++ Practical 1/Question4.java
@@ -0,0 +1,21 @@
+/*
+ * Write a java program to implement adder circuit and booth algorithm
+ * using bitwise operator?
+ */
+class Question4 {
+    public static void main(String[] args) {
+        boolean a, b, c, carry, sum;
+        a = true;
+        b = true;
+        c = false;
+
+        sum = a ^ b;
+        carry = a & b;
+
+        System.out.println("Value of a is: "+a);
+        System.out.println("Value of b is: "+b);
+        System.out.println("Value of c is: "+c);
+        System.out.println("Value of sum is: "+sum);
+        System.out.println("Value of carry is: "+carry);
+    }
+}
--- Practical 1/Question6d.java
+++ Practical 1/Question6d.java
@@ -4,6 +4,22 @@
  */
 class Question6d {
     public static void main(String[] args) {
-        int num;
+        int num, sum;
+        int d1, d2, d3, d4;
+
+        num = 1624;
+        sum = 0;
+
+        d1 = num / 1000;
+        d2 = num / 100 % 10;
+        d3 = num / 10 % 10;
+        d4 = num % 10;
+
+        sum += d1 % 2 == 0 && d2 % 2 == 0  ? d1 * d2 : 0;
+        sum += d2 % 2 == 0 && d3 % 2 == 0  ? d2 * d3 : 0;
+        sum += d3 % 2 == 0 && d4 % 2 == 0  ? d3 * d4 : 0;
+        
+        System.out.println("Sum of product of consecutive even digits: "+sum);
+
     }
 }
